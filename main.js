b75247e5674d739e17a431469d127fdfe197366f
michele camera <michy.cam95@gmail.com>
2022-03-07 15:32:21 +0100

    modify

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 console.log("JS-OK")
 
-const icon = document.querySelector("#header-right a");
+const icon = document.querySelector('#header-right a');
 
 const hiddenmenu = document.getElementsByClassName("hamburger-menu");
 
 icon.addEventListener = ('click', function () {
 
-    console.log(icon, "click")
+    console.log('pulsante cliccato');
 
     if (hiddenmenu.style.display === "none") {
         hiddenmenu.style.display = "none";
