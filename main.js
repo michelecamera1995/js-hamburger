f2f8656bd6fe9cbda70d7a63139b05e099161bc9
michele camera <michy.cam95@gmail.com>
2022-03-07 15:19:48 +0100

    creation javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+
+console.log("JS-OK")
+
+const icon = document.querySelector("#header-right a");
+
+const hiddenmenu = document.getElementsByClassName("hamburger-menu");
+
+icon.addEventListener = ('click', function () {
+
+    console.log(icon, "click")
+
+    if (hiddenmenu.style.display === "none") {
+        hiddenmenu.style.display = "none";
+    } else {
+        hiddenmenu.style.display = "block";
+    }
+
+});
+
